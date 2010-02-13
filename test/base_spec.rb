2d7309c1e25b3e655411bdbac7a10476c5526fa5
Adam Coffman <coffman.adam@gmail.com>
2010-02-13 03:10:22 -0600

    merge

--- test/base_spec.rb
+++ test/base_spec.rb
@@ -19,6 +19,7 @@ describe RapleafApi do
   describe ".new" do 
     it "should initialize RapleafApi from an api key" do
       api = RapleafApi::Api.new( @api_key )
+      api.instance_variable_get(:@API_KEY).should == "?api_key=#{@api_key}"
     end
   end
     
