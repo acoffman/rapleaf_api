b6dcfd3b05caa476dd6d8039630e0f33934a2082
Adam Coffman <coffman.adam@gmail.com>
2010-02-12 21:18:55 -0600

    and were back

---
+++ lib/api/api.rb
@@ -0,0 +1,7 @@
+module RapleafApi
+  class Api
+    def initialize(api_key, options = {})
+      @API_KEY = api_key
+    end
+  end
+end
--- lib/rapleaf_api.rb
+++ lib/rapleaf_api.rb
@@ -1,4 +1,9 @@
 require 'net/http'
+require 'net/http'
+
+%w{api.rb}.each do |file|
+  File.dirname(__FILE__) +"/api/" + file
+end
 
 module RapleafApi
   VERSION = '0.1.0'
